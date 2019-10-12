d6562af6025db3a9fb30d35d30f3dad6ff936978
beto-note <garnauts@gmail.com>
2019-10-12 14:59:52 -0300

    ................

--- lampada_iot.ino
+++ lampada_iot.ino
@@ -1,5 +1,12 @@
-// Programa : Teste Controle Remoto IR
-// Autor : Arduino e Cia
+// Programa : Liga ou Desliga uma lampada, no bocal ou na tomada.
+// Autor : Gilberto Carlos Arnauts
+
+//Pino 4: Inverte as configurações de ligado e desligado.
+
+//Pino 7: Entrada de um interruptor 5 V que quando tem o status alerado, provoca o desligamento ou ligamento da lampada.
+//Pino 11: Entrada do sensor de infravermelho.
+//Pino 12: Liga ou desliga o rele, e o pino 13 tem sempre o status inverso dele.
+//Pino 13: Liga ou desliga o rele, inverso ao 12. 
 
 #include <IRremote.h>
 
@@ -7,32 +14,64 @@ int RECV_PIN = 11;
 float armazenavalor;
 int pinorele = 13;
 int pinorele2 = 12;
+int pininterruptor = 7;
+int pininverteconfiguracoes = 4;
 long milisQueFoiLigado = 0;
+int ultimoStatusInterruptor;
 
 boolean ligado = true;
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-void desliga() {
-  digitalWrite(pinorele, LOW);
-  digitalWrite(pinorele2, HIGH);
+void dlg() {
+  if (digitalRead(pininverteconfiguracoes) == LOW){
+    digitalWrite(pinorele, LOW);
+    digitalWrite(pinorele2, HIGH);
+  } else {
+    digitalWrite(pinorele, HIGH);
+    digitalWrite(pinorele2, LOW);
+  }
   ligado = false;
   Serial.println("Desligado");
 }
 
-void liga() {
-  digitalWrite(pinorele, HIGH);
-  digitalWrite(pinorele2, LOW);
+void lg() {
+  if (digitalRead(pininverteconfiguracoes) == LOW){
+    digitalWrite(pinorele, HIGH);
+    digitalWrite(pinorele2, LOW);
+  } else {
+    digitalWrite(pinorele, LOW);
+    digitalWrite(pinorele2, HIGH);
+  }
   ligado = true;
   milisQueFoiLigado = millis();
   Serial.println("Ligado");
+}
+
+void liga(){
+  if (digitalRead(pininverteconfiguracoes) == LOW){
+    lg();
+  } else {
+    dlg();
+  }
+}
+
+void desliga(){
+   if (digitalRead(pininverteconfiguracoes) == LOW){
+    dlg();
+  } else {
+    lg();
+  }
 }
 
 void setup()
 {
   pinMode(pinorele, OUTPUT);
   pinMode(pinorele2, OUTPUT);
+  pinMode(pininterruptor, INPUT);
+  pinMode(pininverteconfiguracoes, INPUT);
+  ultimoStatusInterruptor = digitalRead(pininterruptor);
   Serial.begin(9600);
   liga();
   irrecv.enableIRIn(); // Inicializa o receptor IR
@@ -44,8 +83,10 @@ void loop()
   //Desliga após algumm tempo esquecido ligado (7200000):
   boolean muitoTempoLigado = ligado == true and (millis() - milisQueFoiLigado) > 7200000;
   if (muitoTempoLigado) Serial.println("Muito tempo ligado...");
-  if (irrecv.decode(&results) or muitoTempoLigado)
+  int statusInterruptor = digitalRead(pininterruptor);
+  if (irrecv.decode(&results) or muitoTempoLigado or statusInterruptor != ultimoStatusInterruptor)
   {
+    ultimoStatusInterruptor = statusInterruptor;
     Serial.print("Valor lido : ");
     Serial.println(results.value, HEX);
     armazenavalor = (results.value);
